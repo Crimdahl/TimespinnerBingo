99be7cca1444056a39c42f13981b4c20032856bf
Crimdahl <crimdahl@outlook.com>
2021-03-20 01:42:09 -0400

    Added Tooltips, Compact Mode

--- BingoBoard.py
+++ BingoBoard.py
@@ -2,7 +2,7 @@ import tkinter
 import os
 import random
 
-class BingoBoard():
+class BingoBoard(object):
     iconDirectory = os.path.join(os.path.dirname(__file__), "Icons")
     consumables = ["Antidote", "Berry Pick-Mi-Up", "Berry Pick-Mi-Up Plus", "Biscuit", "Chaos Rose", "Cheveur Breast", "Cheveur Drumstick",
                         "Dream Wisp", "Eel Meat", "Empress Cake", "Ether", "Filigree Tea", "Hi-Ether", "Hi-Potion", "Jerky", "Mind Refresh Ultra",
@@ -33,11 +33,12 @@ class BingoBoard():
                  useRelics = True, useOrbs = True, usePassives = True, 
                  useSpells = True, useArmors = True, useHeadgear = True, 
                  useTrinkets = True, useKeycards = True, useMiscellaneous = True,
-                 useFamiliars = True, rows = 5, columns = 5):
+                 useFamiliars = True, compactMode = False, rows = 5, columns = 5):
         self.root = tkinter.Tk()
         self.rows = rows
         self.columns = columns
         self.icons = {}
+        self.tooltips = []
 
         if useQuestItems:
             for item in self.quest_items:
@@ -102,16 +103,27 @@ class BingoBoard():
                 frame.grid(row=r, column=c, padx=2, pady=2)
                 randomkey = random.choice(list(self.icons.keys()))
                 icon = self.icons.pop(randomkey)
-                button = tkinter.Button(
-                    master = frame,
-                    text = randomkey,
-                    image = icon,
-                    width = icon.width() * 4,
-                    height = icon.height() * 2,
-                    compound = tkinter.BOTTOM,
-                    bg = "white"
-                )
+                if compactMode:
+                    button = tkinter.Button(
+                        master = frame,
+                        image = icon,
+                        width = icon.width(),
+                        height = icon.height(),
+                        compound = tkinter.BOTTOM,
+                        bg = "white"
+                    )
+                else:
+                    button = tkinter.Button(
+                        master = frame,
+                        text = randomkey,
+                        image = icon,
+                        width = icon.width() * 3.5,
+                        height = icon.height() * 2,
+                        compound = tkinter.BOTTOM,
+                        bg = "white"
+                    )
                 button.config(command = lambda arg=button:buttonClick(arg))
+                self.tooltips.append(ToolTip(button, randomkey))
                 button.image = icon
                 button.pack()
 
@@ -121,86 +133,56 @@ class BingoBoard():
                 arg["bg"] = "green"
             elif arg["bg"] == "green":
                 arg["bg"] = "white"
+    
+class ToolTip(object):
+    def __init__(self, widget, text="widget info"):
+        self.waittime = 0
+        self.wraplength = 180
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+        self.widget.bind("<ButtonPress>", self.leave)
+        self.id = None
+        self.tw = None
 
+    def enter(self, event=None):
+        self.schedule()
 
-    def loadQuestItems(self):
-        for item in self.quest_items:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
-                
-    def loadRelics(self):
-        for item in self.relics:
-                if item not in self.icons.keys():
-                    self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
+    def leave(self, event=None):
+        self.unschedule()
+        self.hidetip()
 
-    def loadOrbs(self):
-        for item in self.orbs:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
+    def schedule(self):
+        self.unschedule()
+        self.id = self.widget.after(self.waittime, self.showtip)
 
-    def loadPassives(self):
-        for item in self.passives:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
+    def unschedule(self):
+        id = self.id
+        self.id = None
+        if id:
+            self.widget.after_cancel(id)
 
-    def loadSpells(self):
-        for item in self.spells:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
+    def showtip(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() #- (self.widget.winfo_width() / 2)
+        y += self.widget.winfo_rooty() - (self.widget.winfo_height() / 2)
+        # creates a toplevel window
+        self.tw = tkinter.Toplevel(self.widget)
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = tkinter.Label(self.tw, text=self.text, justify='left',
+                        background="#ffffff", relief='solid', borderwidth=1,
+                        wraplength = self.wraplength)
+        label.pack(ipadx=1)
 
-    def loadArmors(self):
-        for item in self.armors:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
-
-    def loadHeadgear(self):
-        for item in self.headgear:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
-
-    def loadTrinkets(self):
-        for item in self.trinkets:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
-
-    def loadKeycards(self):
-        for item in self.keycards:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
-
-    def loadFamiliars(self):
-        for item in self.familiars:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
-
-    def loadMiscellaneous(self):
-        for item in self.miscellaneous:
-            if item not in self.icons.keys():
-                self.icons[item] = tkinter.PhotoImage(file = os.path.join(self.iconDirectory, item)).zoom(2)
-
-    def displayGrid(self):
-        for c in range(COLUMNS):
-            for r in range(ROWS):
-                frame = tkinter.Frame(
-                    master = self
-
-                )
-                frame.grid(row=r, column=c, padx=2, pady=2)
-                randomkey = random.choice(list(self.icons.keys()))
-                icon = self.icons.pop(randomkey)
-                button = tkinter.Button(
-                    master = frame,
-                    text = randomkey,
-                    image = icon,
-                    width = icon.width() * 4,
-                    height = icon.height() * 2,
-                    compound = tkinter.BOTTOM,
-                    bg = "white"
-                )
-                button.config(command = lambda arg=button:buttonClick(arg))
-                button.image = icon
-                button.pack()
-
+    def hidetip(self):
+        tw = self.tw
+        self.tw= None
+        if tw:
+            tw.destroy()
     
 
 
--- TimespinnerBingo.py
+++ TimespinnerBingo.py
@@ -1,62 +1,8 @@
 import tkinter
-import os
-import random
-import sys
 import BingoBoard
 
-#WINDOW = None
-
-#ROWS = 5
-#COLUMNS = 5
-#ICONDIR = os.path.join(os.path.dirname(__file__), "Icons")
-#ICONSET = {}
-#CONSUMABLE_SET = set()
-#QUEST_SET = set()
-#RELIC_SET = set()
-#ORB_SET = set()
-#PASSIVE_SET = set()
-#SPELL_SET = set()
-#ARMOR_SET = set()
-#HEADGEAR_SET = set()
-#TRINKET_SET = set()
-#KEYCARD_SET = set()
-
-def buttonClick(self):
-    if self["bg"] == "white":
-        self["bg"] = "green"
-    elif self["bg"] == "green":
-        self["bg"] = "white"
 
 if __name__ == "__main__":
-    #WINDOW = tkinter.Tk()
-    #WINDOW.title("Timespinner Bingo")
-
-    #for filename in os.listdir(ICONDIR):
-    #    if filename.endswith(".png"):
-    #        ICONSET[filename[:filename.index(".")]] = tkinter.PhotoImage(file = os.path.join(ICONDIR, filename), name = "Hello").zoom(2)
-
-    #for c in range(COLUMNS):
-    #    for r in range(ROWS):
-    #        frame = tkinter.Frame(
-    #            master = WINDOW
-
-    #        )
-    #        frame.grid(row=r, column=c, padx=2, pady=2)
-    #        randomkey = random.choice(list(ICONSET.keys()))
-    #        icon = ICONSET.pop(randomkey)
-    #        button = tkinter.Button(
-    #            master = frame,
-    #            text = randomkey,
-    #            image = icon,
-    #            width = icon.width() * 4,
-    #            height = icon.height() * 2,
-    #            compound = tkinter.BOTTOM,
-    #            bg = "white"
-    #        )
-    #        button.config(command = lambda arg=button:buttonClick(arg))
-    #        button.image = icon
-    #        button.pack()
-
-    #WINDOW.mainloop()
     window = BingoBoard.BingoBoard()
+    window.root.title("Bingo")
     window.root.mainloop()
